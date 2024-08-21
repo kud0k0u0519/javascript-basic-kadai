2a82222f087106bd6125121006c7cc86d03a2be7
kud0k0u0519 <kud0k0u.5519@gmail.com>
2024-08-21 11:55:25 +0900

    課題データの再アップロード

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,8 +1,9 @@
 // btnというidを持つHTML要素を取得し、定数に代入する
 const addBtn = document.getElementById('btn');
 
+const changeText = document.getElementById('text');
+
 //上記で代入したボタンをクリックした時にイベント処理を実行する
 addBtn.addEventListener('click', () => {
-  // id名textを取得し、その要素内のテキストを変更する
-  document.getElementById('text').textContent = 'ボタンをクリックしました'
-})
+  changeText.textContent = 'ボタンをクリックしました'
+});
